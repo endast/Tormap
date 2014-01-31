bc886f89ec0e7571657d92ebc35c36610652b257
Magnus Wahlberg <magnus@RedSwampResearch-Alpha.local>
2014-01-31 07:30:53 +0100

    First

---
+++ app.js
@@ -0,0 +1,34 @@
+var fs = require('fs');
+var readline = require('readline');
+var geoip = require('geoip-lite');
+
+var rawData = process.argv[2];
+
+if(!rawData){
+    console.log("No input!");
+    process.exit(1);
+}
+
+console.log("ip|name|router-port|directory-port|flags|uptime|version|contactinfo|country|region|city|latitude|longitue");
+
+var rd = readline.createInterface({
+    input: fs.createReadStream(rawData),
+    output: process.stdout,
+    terminal: false
+});
+
+rd.on('line', function(line) {
+    var lineParts = line.split('|');
+
+    // Fix rows with pipes in contact info
+    if(lineParts.length>8){
+        var contact = lineParts.slice(7).join("");
+        var parts = lineParts.slice(0,7);
+        parts.push(contact);     
+        line = parts.join("|");
+    }
+
+    var geo = geoip.lookup(lineParts[0]);
+
+    console.log(line+"|"+geo.country+"|"+geo.region+"|"+geo.city+"|"+geo.ll[0]+"|"+geo.ll[1]);
+});
---
+++ getData.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl –silent https://www.dan.me.uk/tornodes 2>/dev/null | sed -n "/<\!-- __BEGIN_TOR_NODE_LIST__ \/\/-->/,/<\!-- __END_TOR_NODE_LIST__ \/\/-->/p" | sed '1d;$d' | sed 's/<br \/>//g' > torlist.txt
